f337f4bd6ac58913a9e834eed4a3f3265c852428
JosephJoshua <jj.anggita@gmail.com>
2026-03-27 17:22:00 +0000

    feat(user): add 14 tangram user programs and register in cases.toml

---
+++ tg-rcore-tutorial-user/src/bin/tangram_01.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user_lib;
+
+#[unsafe(no_mangle)]
+extern "C" fn main() -> i32 {
+    let idx: usize = 1;
+    println!("[tangram] rendering piece {}: {}", idx, user_lib::tangram::PIECE_NAMES[idx]);
+    user_lib::tangram::render_piece(idx);
+    user_lib::tangram::spin_wait_ms(300);
+    0
+}
